fbc6c15a3fb46a59890e564bb87e15cfff4ac66d
임재상 <jaesang@zuminternet.com>
2019-02-21 18:26:43 +0900

    URL 처리시 필요할 것 같은 함수

---
+++ src/main/scala/url/ParameterExtractor.scala
@@ -0,0 +1,68 @@
+package url
+
+import java.net.{URL, URLDecoder}
+import java.util.regex.Pattern
+
+/**
+  * Created by jaesang on 2019-02-21.
+  */
+object ParameterExtractor {
+  val DEFAULT_ENCODING = "UTF-8"
+
+  def getParameterValue(url: String, param: String): Option[String] = {
+    try {
+
+      if (url == null) None
+
+      val urlObject = new URL(url.toLowerCase())
+      val host = urlObject.getHost
+      if (host == null) None
+
+      var queryString = urlObject.getQuery
+      val index = url.indexOf(urlObject.getHost)
+      /**
+        * getQuery() 로 query 관련 파라미터들이 없는 경우가 있어
+        * Host가 끝나는 지점 이후의 파라미터들을 모두 사용
+        */
+      if (index > -1) {
+        val start = index + urlObject.getHost.length
+        queryString = url.substring(start)
+      }
+
+      val queryMatcher = Pattern.compile("\\b" + param + "=([^&]+)")
+        .matcher(queryString)
+
+      if (!queryMatcher.find) None
+
+      val query = queryMatcher.group(1)
+      Some(URLDecoder.decode(query, DEFAULT_ENCODING))
+
+    } catch {
+      case ex: Throwable => {
+        ex.printStackTrace()
+        None
+      }
+    }
+
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    /**
+      * 검색 쿼리관련한 파라미터는 각 포털마다 다르다
+      * search.zum.com / query
+      * search.naver.com / query
+      * search.daum.net / q
+      * google.com / q
+      */
+
+    val url = "http://search.zum.com/search.zum?method=uni&option=accu&qm=f_typing&rd=1&query=4%EC%9B%94+11%EC%9D%BC+%EC%9E%84%EC%8B%9C%EA%B3%B5%ED%9C%B4%EC%9D%BC"
+    val param = "query"
+
+    getParameterValue(url, param) match {
+      case Some(query) => println(query)
+      case _ => println("error")
+    }
+  }
+
+}
